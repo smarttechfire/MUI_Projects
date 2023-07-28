9e798cccbf7380fbb162ca9ffa232bcaadc86b59
smarttechfire <educationfire552@gmail.com>
2023-07-28 13:19:56 +0530

    finish NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,75 @@
-
-import { Box, Typography, Container, styled } from "@mui/material";
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
-
+import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
+import MiscellaneousServicesIcon from "@mui/icons-material/MiscellaneousServices";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import HomeIcon from "@mui/icons-material/Home";
+import ContactsIcon from "@mui/icons-material/Contacts";
 import logoImg from "../media/logo.png";
-import CustomButton from "../components/CustomButton";
+import { Container } from "@mui/system";
+import CustomButton from "./CustomButton";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+} from "@mui/material";
+import { useState } from "react";
 const Navbar = () => {
+
+   const [mobileMenu, setMobileMenu] = useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor,open) => (event) =>{
+    if(
+      event.type === "keydown" && 
+      (event.type === "Tab" || event.type === "Shift"))
+    {
+      return;
+    }
+    setMobileMenu({ ...mobileMenu,[anchor]: open});
+  };
+
+
+
+  const list = (anchor) => (
+    <Box sx={{
+      width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
+      height: "100%",
+      background: "#E6F0FF",
+      color:"#000"
+    }} 
+      role="presentation"
+      onClick={toggleDrawer(anchor,false)}
+      onKeyDown = {toggleDrawer(anchor,false)}
+      >
+        <List>
+          {["Home","Features","Services","Listed","Contact"].map(
+            (text,index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index === 0 && <HomeIcon />}
+                    {index === 1 && <FeaturedPlayListIcon />}
+                    {index === 2 && <MiscellaneousServicesIcon />}
+                    {index === 3 && <ListAltIcon />}
+                    {index === 4 && <ContactsIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            )
+          )}
+        </List>
+    </Box>
+  );
+
   const NavLink = styled(Typography)(({ theme }) => ({
     fontSize: "14px",
     color: "#4F5361",
@@ -70,7 +134,14 @@ const Navbar = () => {
         display:"flex",
         alignItems:"center",
       }}>
-        <CustomMenuIcon />
+        <CustomMenuIcon onClick={toggleDrawer("left",true)} />
+        <Drawer
+          anchor="left"
+          open={mobileMenu["left"]}
+          onClose={toggleDrawer("left",false)}
+        >
+          {list("left")}
+        </Drawer>
         <NavbarLogo src={logoImg} alt="logo" />
       </Box>
       <NavbarLinksBox>
@@ -79,7 +150,6 @@ const Navbar = () => {
         <NavLink variant="body2">Services</NavLink>
         <NavLink variant="body2">Listed</NavLink>
         <NavLink variant="body2">Contact</NavLink>
-        <NavLink variant="body2">Gallery</NavLink>
       </NavbarLinksBox>
     </Box>
     <Box sx={{display:"flex",alignItems:"center",justifyContent:"center",gap:"1rem"}}> 
